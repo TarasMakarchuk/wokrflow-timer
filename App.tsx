3cef3d37a8fcce8071c6f05aed8a7d8f20aa7ce4
Taras Makarchuk <2278452@gmail.com>
2023-08-15 12:32:27 +0300

    :package: add react navigation npms

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import { Text, View } from 'react-native';
 
 export default function () {
   return (
-    <View className='bg-[#1E1C2E] w-full'>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View className='bg-[#1E1C2E]'>
+        <Text className='text-white'>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
   );
 }
