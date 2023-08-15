314a230744b696e9da666eb6a1df891925e6ec39
Taras Makarchuk <2278452@gmail.com>
2023-08-15 13:58:28 +0300

    :building_construction: add routing and routes

---
+++ app/components/screens/settings/Settings.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { Text, View } from 'react-native';
+
+export const Settings: FC = () => {
+  return (
+    <View>
+      <Text>Settings</Text>
+    </View>
+  );
+};
---
+++ app/navigation/Navigation.tsx
@@ -0,0 +1,27 @@
+import { FC, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import { useAuth } from '@/hooks/useAuth';
+import { useNavigationContainerRef } from '@react-navigation/native';
+
+export const Navigation: FC = () => {
+  const { user } = useAuth();
+
+  const [currentRoute, setCurrentRoute] = useState<string | undefined>(undefined);
+
+  const navRef = useNavigationContainerRef();
+
+  useEffect(() => {
+    setCurrentRoute(navRef.getCurrentRoute()?.name);
+    const listener = navRef.addListener('state', () => setCurrentRoute(navRef.getCurrentRoute()?.name));
+
+    return () => {
+      navRef.removeListener('state', () => listener);
+    }
+  }, []);
+
+  return (
+    <View>
+      <Text>Navigation</Text>
+    </View>
+  );
+};
