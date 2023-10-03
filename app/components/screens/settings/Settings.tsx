5617d3b9d97135dfc7bd870c49acfade33ea0740
Taras Makarchuk <2278452@gmail.com>
2023-10-03 20:50:11 +0300

    :construction: update Settings.tsx file

--- app/components/screens/settings/Settings.tsx
+++ app/components/screens/settings/Settings.tsx
@@ -4,7 +4,9 @@ import { Text, View } from 'react-native';
 export const Settings: FC = () => {
   return (
     <View>
-      <Text>Settings</Text>
+      <Text>
+        Settings
+      </Text>
     </View>
   );
 };
